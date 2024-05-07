d14e94cc977b3fe4aca962122edb81ec3b82fa8a
jorge-js <167325164+jorge-js@users.noreply.github.com>
2024-05-07 15:54:00 +0000

    guardado 8

--- app.js
+++ app.js
@@ -1,39 +1,67 @@
-
+// Importando el modulo http
+import http from 'http';
 // Importando Express
 import express from 'express';
+import { log } from 'console';
 
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
-     //////////////////////////////////////////////////////    
+
+// Se registra el middleware del body-parser
+app.use(express.urlencoded({extended: true}));
+
 // Ruta about
 // GET /about
 app.use('/about',(req, res)=>{
-    console.log("📢 Sirviendo la ruta '/about'");
-    // Se contesta al server
-    res.send(`
-      <h1>🪄 About...</h1>
-      <p>App for Fullstack Web Dev Course I!</p>
-    `);
-  });
-  
-  // Ruta Raíz
-  // GET /
-  app.use((req, res)=>{
-    console.log("📢 Sirviendo la ruta '/'");
-    // Se contesta al server
-    res.send(`
-      <h1>Welcome to Express Js</h1>
-      <p>This is my awesome app! 😎</p>
-    `);
-  });
-      /////////////////////////////////////////////////////////7
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
+
+// GET '/add-product'
+app.get('/add-product', (req, res, next) => {
+  // Si la petición es post pasamos el siguiente
+  // Middleware
+  if(req.method === "POST") return next();
+
+  // Servimos el formulario
+  console.log("📢 Sirviendo formulario...");
+  res.send(`
+  <form action="/add-product" method="POST">
+    <input type="text" name="title">
+    <button type="submit">Add product</button>
+  </form>
+  `);
+});
+
+// POST '/add-product'
+app.post('/add-product', (req, res)=>{
+  // Realizaremos la extracción de
+  // parametros dentro de la peticion
+  console.log(req.body);
+  res.redirect('/');
+});
+
+// Ruta Raíz
+// GET /
+app.use((req, res)=>{
+  console.log("📢 Sirviendo la ruta '/'");
+  // Se contesta al server
+  res.send(`
+    <h1>Welcome to Express Js</h1>
+    <p>This is my awesome app! 😎</p>
+  `);
+});
+
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
 
 // Arrancando el servidor
-app.listen(port, ip, (err) => {  
-       if(err) console.log("error al arrancar el server");   
+app.listen(port, ip, () => {
   console.log(`🤖 Sirviendo en http://localhost:${port}`);
 });
