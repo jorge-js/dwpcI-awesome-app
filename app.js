291eefb68631386da131f17ec2b2879f5d3cbe7e
jorge-js <167325164+jorge-js@users.noreply.github.com>
2024-05-07 14:47:45 +0000

    cambio 5

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ import express from 'express';
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
+             // Registrando el primer middleware
+app.use((req, res, next) => {
+   console.log("Middleware #1");
+      // Se invoca al siguiente middlware
+     next();
+});
+     // Registrando el segundo middleware
+app.use((req, res, next)=>{
+    console.log("Middleware #2");
+    // Se invoca al siguiente middlware
+    res.send(`
+    <h1>Welcome to Express Js</h1>
+    <p>This is my awesome app! </p>
+      `);
+  });
 
 // Asignando el middleware de express
 // al servidor http
