457099ee67664f67c4fc8a8c12bf1e0c62f6fb02
jorge-js <167325164+jorge-js@users.noreply.github.com>
2024-05-20 14:16:38 +0000

    vistas estaticas 1

--- app.js
+++ app.js
@@ -18,62 +18,6 @@ app.use(adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
 
-/*
-// Ruta about
-// GET /about
-app.use('/about',(req, res)=>{
-  console.log("📢 Sirviendo la ruta '/about'");
-  // Se contesta al server
-  res.send(`
-    <h1>🪄 About...</h1>
-    <p>App for Fullstack Web Dev Course I!</p>
-  `);
-});
-    */   
-
-//codigo eliminado empieza aqui 📢
-/*
-// GET '/add-product'
-app.get('/add-product', (req, res, next) => {
-  // Si la petición es post pasamos el siguiente
-  // Middleware
-  if(req.method === "POST") return next();
-     */
-/*
-  // Servimos el formulario
-  console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
-});
-      */
-     /*
-// POST '/add-product'
-app.post('/add-product', (req, res)=>{
-  // Realizaremos la extracción de
-  // parametros dentro de la peticion
-  console.log(req.body);
-  res.redirect('/');
-});
-      */
-//codigo eliminado termina aqui 📢
-
-// Ruta Raíz
-// GET /
-  /*
-app.use((req, res)=>{
-  console.log("📢 Sirviendo la ruta '/'");
-  // Se contesta al server
-  res.send(`
-    <h1>Welcome to Express Js</h1>
-    <p>This is my awesome app! 😎</p>
-  `);
-});
-    */
- 
 // Definiendo puertos
 const port = 3000;
 const ip = "0.0.0.0"
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,19 +1,20 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-
+   // Importando el gestor de rutas
+import path from 'path';
 // Creando una instancia del enrutador de express
 const router = Router();
 
 // GET /
 router.get('/', (req, res)=>{
   console.log("📢 Sirviendo la ruta '/'");
+  res.sendFile(path.resolve('views','shop.html'));
   // Se contesta al server
   res.send(`
     <h1>Welcome to Express Js</h1>
     <p>This is my awesome app! 😎</p>
   `);
 });
-
 // GET /about
 router.get('/about', (req, res) => {
   console.log("📢 Sirviendo la ruta '/about'");
@@ -23,5 +24,4 @@ router.get('/about', (req, res) => {
     <p>App for Fullstack Web Dev Course I!</p>
   `);
 });
-
 export default router;
