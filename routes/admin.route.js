c2b7ed9f9fc4fc4a393bdf86ac281a236f778fc3
jorge-js <167325164+jorge-js@users.noreply.github.com>
2024-05-20 16:31:19 +0000

    auxiliares para navegacion

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,25 +1,21 @@
 // Importando el enrutador de express
 import { Router } from 'express';
 
+// Importando el gestor de rutas
+import path from 'path';
+import { ROOT_DIR } from '../helpers/paths.js';
+
 // Creando una instancia del enrutador de express
 const router = Router();
 
 // GET /add-product
-
 router.get('/add-product', (req, res, next) => {
-  // Si la petición es post pasamos el siguiente
-  // Middleware
-  if(req.method === "POST") return next();
-
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  res.send(`
-  <form action="/add-product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add product</button>
-  </form>
-  `);
+  console.log(`📢 ROOT_DIR: ${ROOT_DIR}`);
+  res.sendFile(path.join(ROOT_DIR, 'views','add-product.html'));
 });
+
 // POST /add-product
 router.post('/add-product', (req, res) => {
   // Realizaremos la extracción de
